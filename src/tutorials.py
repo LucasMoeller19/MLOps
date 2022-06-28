107b2ef424292344c313a045f8d58ca2013e3694
Lucas Möller <lucas.moeller1995@yahoo.de>
2022-06-28 09:41:56 +0200

    testing Dask

--- src/tutorials.py
+++ src/tutorials.py
@@ -1,7 +1,17 @@
+"""
 import prefect
 from prefect import task, Flow
+"""
+import numpy as np
+import pandas as pd
+
+import dask.dataframe as dd
+import dask.array as da
+import dask.bag as db
 
 
+### Prefect ###
+"""
 @task
 def hello_task():
     logger = prefect.context.get("logger")
@@ -12,3 +22,12 @@ with Flow("hello-world") as flow:
     hello_task()
 
 flow.run()
+"""
+
+### Dask ###
+index = pd.date_range("2021-09-01", periods=2400, freq="1H")
+df = pd.DataFrame({"a": np.arange(2400), "b": list("abcaddbe" * 300)}, index=index)
+ddf = dd.from_pandas(df, npartitions=10)
+
+
+print(ddf.b.unique().compute())
